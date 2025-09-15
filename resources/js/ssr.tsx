ad978e6866c522cfba4758ae2d66651f608c5457
msikkc21 <izzaaida21@gmail.com>
2025-09-15 20:39:03 +0700

    first commit

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -9,10 +9,8 @@ createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
-        title: (title) => (title ? `${title} - ${appName}` : appName),
-        resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
-        setup: ({ App, props }) => {
-            return <App {...props} />;
-        },
+        title: (title) => title ? `${title} - ${appName}` : appName,
+        resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+        setup: ({ App, props }) => <App {...props} />,
     }),
 );
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,32 +1,11 @@
-import { InertiaLinkProps } from '@inertiajs/react';
-import { LucideIcon } from 'lucide-react';
-
 export interface Auth {
     user: User;
-}
-
-export interface BreadcrumbItem {
-    title: string;
-    href: string;
-}
-
-export interface NavGroup {
-    title: string;
-    items: NavItem[];
-}
-
-export interface NavItem {
-    title: string;
-    href: NonNullable<InertiaLinkProps['href']>;
-    icon?: LucideIcon | null;
-    isActive?: boolean;
 }
 
 export interface SharedData {
     name: string;
     quote: { message: string; author: string };
     auth: Auth;
-    sidebarOpen: boolean;
     [key: string]: unknown;
 }
 
