5bdb1b231167a9d9743e6b7f4d371faa0ded6fed
msikkc21 <izzaaida21@gmail.com>
2025-09-22 14:21:40 +0700

    add laravel breeze

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,8 +7,8 @@ import { createRoot } from 'react-dom/client';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
