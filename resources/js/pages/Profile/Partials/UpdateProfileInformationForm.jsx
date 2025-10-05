30e7fb3476b20672f592f7fa60d6f4509fed3c9c
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-10-05 16:18:53 +0700

    fix typo name to nama

--- resources/js/pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -14,7 +14,7 @@ export default function UpdateProfileInformation({
 
     const { data, setData, patch, errors, processing, recentlySuccessful } =
         useForm({
-            name: user.name,
+            name: user.nama,
             email: user.email,
         });
 
