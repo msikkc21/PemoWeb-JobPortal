c57f9ba347f8e81a2672410be244d616634e04d6
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-10-05 17:44:23 +0700

    change back nama to name

--- resources/js/pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -14,7 +14,7 @@ export default function UpdateProfileInformation({
 
     const { data, setData, patch, errors, processing, recentlySuccessful } =
         useForm({
-            name: user.nama,
+            name: user.name,
             email: user.email,
         });
 
