ce4e273d994e370e383ecbbf5ddd799d99a113a6
Fadhlu Ibnu <f.ibnu.a1@gmail.com>
2025-11-03 10:20:11 +0700

    feat: Add middleware for active subscription to job creation and CRUD routes

--- routes/modules/company.php
+++ routes/modules/company.php
@@ -29,65 +29,66 @@ Route::middleware(['auth', 'check.permission:company', 'ensure.profile'])->group
     // Profile routes
     Route::get('/company/profile', [ProfileController::class, 'show'])
         ->name('company.profile.show');
-    
+
     Route::get('/company/profile/edit', [ProfileController::class, 'edit'])
         ->name('company.profile.edit');
-    
+
     Route::post('/company/profile/update', [ProfileController::class, 'update'])
         ->name('company.profile.update');
 
     // Subscription routes (Point 7 & 8: Lihat Status & Pilih Paket)
     Route::get('/company/subscription', [SubscriptionController::class, 'index'])
         ->name('company.subscription.index');
-    
+
     Route::get('/company/subscription/choose', [SubscriptionController::class, 'choose'])
         ->name('company.subscription.choose');
-    
+
     Route::post('/company/subscription/activate', [SubscriptionController::class, 'activate'])
         ->name('company.subscription.activate');
-    
+
     Route::get('/company/subscription/invoice/{id}', [SubscriptionController::class, 'invoice'])
         ->name('company.subscription.invoice');
 
     // Payment routes
     Route::post('/company/subscription/payment/create', [SubscriptionController::class, 'createPayment'])
         ->name('company.subscription.payment.create');
-    
+
     Route::get('/company/subscription/payment/continue', [SubscriptionController::class, 'continuePayment'])
         ->name('company.subscription.payment.continue');
-    
+
     Route::get('/company/subscription/payment/check/{external_id}', [SubscriptionController::class, 'checkPayment'])
         ->name('company.subscription.payment.check');
-    
+
+    Route::get('/company/jobs/create', [\App\Http\Controllers\Company\JobController::class, 'create'])
+        ->name('company.jobs.create')->middleware('ensure.active.subscription');
+
     // Jobs routes - Read-only (no subscription required)
     Route::get('/company/jobs', [\App\Http\Controllers\Company\JobController::class, 'index'])
         ->name('company.jobs.index');
-    
+
     Route::get('/company/jobs/{job}', [\App\Http\Controllers\Company\JobController::class, 'show'])
         ->name('company.jobs.show');
-    
+
     // Jobs CRUD routes - Require active subscription
     Route::middleware('ensure.active.subscription')->group(function () {
-        Route::get('/company/jobs/create', [\App\Http\Controllers\Company\JobController::class, 'create'])
-            ->name('company.jobs.create');
-        
+
         Route::post('/company/jobs', [\App\Http\Controllers\Company\JobController::class, 'store'])
             ->name('company.jobs.store');
-        
+
         Route::get('/company/jobs/{job}/edit', [\App\Http\Controllers\Company\JobController::class, 'edit'])
             ->name('company.jobs.edit');
-        
+
         Route::put('/company/jobs/{job}', [\App\Http\Controllers\Company\JobController::class, 'update'])
             ->name('company.jobs.update');
-        
+
         Route::patch('/company/jobs/{job}', [\App\Http\Controllers\Company\JobController::class, 'update']);
-        
+
         Route::delete('/company/jobs/{job}', [\App\Http\Controllers\Company\JobController::class, 'destroy'])
             ->name('company.jobs.destroy');
-        
+
         Route::post('/company/jobs/{job}/submit', [\App\Http\Controllers\Company\JobController::class, 'submitForReview'])
             ->name('company.jobs.submit');
-        
+
         Route::post('/company/jobs/{job}/close', [\App\Http\Controllers\Company\JobController::class, 'close'])
             ->name('company.jobs.close');
     });
